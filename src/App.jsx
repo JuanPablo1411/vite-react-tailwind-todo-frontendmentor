3f6573f57eeaaede1572b2e62eb5a8b4d477e07a
Juan <juampi.mantegazza@gmail.com>
2023-08-28 12:53:55 -0300

    update localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -68,10 +68,10 @@ const App = () => {
   };
 
   return (
-    <div className=" transition-all duration-1000 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-contain dark:bg-gray-900 dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] bg-gray-300 min-h-screen">
+    <div className=" transition-all duration-1000 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-contain dark:bg-gray-900 dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] bg-gray-300 min-h-screen md:bg-[url('./assets/images/bg-desktop-light.jpg')] md:dark:bg-[url('./assets/images/bg-desktop-dark.jpg')]">
       <Header />
 
-      <main className="container mx-auto px-4 mt-8">
+      <main className="container mx-auto px-4 mt-8 md:max-w-xl">
         <TodoCreate createTodo={createTodo} />
 
         <TodoList
